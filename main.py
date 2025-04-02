920c89ff5cfbed5dc202bffde3bb740cf6e314ed
hsommer990 <hsommer990@gmail.com>
2025-04-02 14:48:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,9 @@ for classes in list_classes :
 # HALEY SOMMER - TASK 3 - Each column should have an excel filter element above it
 
 # Use the previously written class for loop as a basis for looping these columns in each sheet
+    max_row = formatted_workbook[classes].max_row
     last_col = get_column_letter(formatted_workbook[classes].max_column) # use the openpxyl utility get_column_letter to fill in the last column letter
-    formatted_workbook[classes].auto_filter.ref = f"A1:{last_col}4" # use auto_filter.ref to add filters to the first 4 columns
+    formatted_workbook[classes].auto_filter.ref = f"A1:{last_col}{max_row}" # use auto_filter.ref to add filters to the first 4 columns
 
 # Rebecca - TASK 4 - Each sheet should have summary information
 # Use functions to calculate the following data
